fe60103f869df02c7c283d573c1329485eea7575
ylem76 <ylem76@gmail.com>
2024-11-11 20:47:53 +0900

    refactor: if문은 함수의 시작에만 있도록 분리

--- index.ts
+++ index.ts
@@ -91,37 +91,42 @@ function moveVertical(dy: number) {
     moveToTile(playerx, playery + dy);
   }
 }
-
+const handleInput = (current: Input) => {
+  if (current === Input.LEFT) moveHorizontal(-1);
+  else if (current === Input.RIGHT) moveHorizontal(1);
+  else if (current === Input.UP) moveVertical(-1);
+  else if (current === Input.DOWN) moveVertical(1);
+};
 const handleInputs = (inputs: Input[]) => {
   while (inputs.length > 0) {
     let current = inputs.pop();
-    if (current === Input.LEFT) moveHorizontal(-1);
-    else if (current === Input.RIGHT) moveHorizontal(1);
-    else if (current === Input.UP) moveVertical(-1);
-    else if (current === Input.DOWN) moveVertical(1);
+    handleInput(current);
   }
 };
 
+const updateTile = (map: Tile[][], x: number, y: number) => {
+  if (
+    (map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE) &&
+    map[y + 1][x] === Tile.AIR
+  ) {
+    map[y + 1][x] = Tile.FALLING_STONE;
+    map[y][x] = Tile.AIR;
+  } else if (
+    (map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX) &&
+    map[y + 1][x] === Tile.AIR
+  ) {
+    map[y + 1][x] = Tile.FALLING_BOX;
+    map[y][x] = Tile.AIR;
+  } else if (map[y][x] === Tile.FALLING_STONE) {
+    map[y][x] = Tile.STONE;
+  } else if (map[y][x] === Tile.FALLING_BOX) {
+    map[y][x] = Tile.BOX;
+  }
+};
 const updateMap = (map: Tile[][]) => {
   for (let y = map.length - 1; y >= 0; y--) {
     for (let x = 0; x < map[y].length; x++) {
-      if (
-        (map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE) &&
-        map[y + 1][x] === Tile.AIR
-      ) {
-        map[y + 1][x] = Tile.FALLING_STONE;
-        map[y][x] = Tile.AIR;
-      } else if (
-        (map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX) &&
-        map[y + 1][x] === Tile.AIR
-      ) {
-        map[y + 1][x] = Tile.FALLING_BOX;
-        map[y][x] = Tile.AIR;
-      } else if (map[y][x] === Tile.FALLING_STONE) {
-        map[y][x] = Tile.STONE;
-      } else if (map[y][x] === Tile.FALLING_BOX) {
-        map[y][x] = Tile.BOX;
-      }
+      updateTile(map, x, y);
     }
   }
 };
