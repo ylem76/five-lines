f9594d66e1b9d8bb7bec8a406af1adf402850a12
ylem76 <ylem76@gmail.com>
2024-11-11 21:49:27 +0900

    refactor: 클래스로 타입 코드 대체

--- index.ts
+++ index.ts
@@ -18,69 +18,30 @@ enum Tile {
 }
 
 interface Input {
-  isRight(): boolean;
-  isLeft(): boolean;
-  isUp(): boolean;
-  isDown(): boolean;
+  handle(): void;
 }
 
 class Right implements Input {
-  isRight() {
-    return true;
-  }
-  isLeft() {
-    return false;
-  }
-  isUp() {
-    return false;
-  }
-  isDown() {
-    return false;
+  handle() {
+    moveHorizontal(1);
   }
 }
 
 class Left implements Input {
-  isRight() {
-    return false;
-  }
-  isLeft() {
-    return true;
-  }
-  isUp() {
-    return false;
-  }
-  isDown() {
-    return false;
+  handle() {
+    moveHorizontal(-1);
   }
 }
 
 class Up implements Input {
-  isRight() {
-    return false;
-  }
-  isLeft() {
-    return false;
-  }
-  isUp() {
-    return true;
-  }
-  isDown() {
-    return false;
+  handle() {
+    moveVertical(-1);
   }
 }
 
 class Down implements Input {
-  isRight() {
-    return false;
-  }
-  isLeft() {
-    return false;
-  }
-  isUp() {
-    return false;
-  }
-  isDown() {
-    return true;
+  handle() {
+    moveVertical(1);
   }
 }
 let playerx = 1;
@@ -151,10 +112,7 @@ function moveVertical(dy: number) {
   }
 }
 const handleInput = (input: Input) => {
-  if (input.isLeft()) moveHorizontal(-1);
-  else if (input.isRight()) moveHorizontal(1);
-  else if (input.isUp()) moveVertical(-1);
-  else if (input.isDown()) moveVertical(1);
+  input.handle();
 };
 
 const handleInputs = (inputs: Input[]) => {
